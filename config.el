bae706626b6ccf439e5a8e8179109ce94acff5a1
Zafar Takhirov <cc.rafaz@zafar.cc>
2015-08-14 19:05:06 -0400

    Initial custom setup

---
+++ config.el
@@ -0,0 +1,40 @@
+
+;; ----------------------------------------------------------------------------
+;; Custom configuration from Prelude documentation
+;; ----------------------------------------------------------------------------
+
+;; Disable whitespace mode
+(setq prelude-whitespace nil)
+
+;; Disable auto-cleanup of whitespaces on save
+; (setq prelude-clean-whitespace-on-save nil)
+
+;; Disable spellchecking on the fly
+; (setq prelude-flyspell nil)
+
+;; Disable arrow keys completely
+; (setq guru-warn-only nil)
+
+;; Disable guru-mode completely
+(setq prelude-guru nil)    ; I am not an Emacs guru :(
+
+;; Set C-a as a default (goto first column)
+; (global-set-key [remap move-beginning-of-line] 'move-beginning-of-line)
+
+;; If you are searching in large databases and your machine slow uncomment 
+;; the following
+; (setq flx-ido-threshold 1000)
+
+;; Or disable the improved sorting algorithm
+; (flx-ido-mode -1)
+
+
+;; ----------------------------------------------------------------------------
+;; My custom configuration
+;; ----------------------------------------------------------------------------
+
+;; Enable the line numbers by default
+(global-linum-mode 1)
+
+;; Separate line numbers from text
+(setq linum-format "%d ")
---
+++ preload/init.el
@@ -0,0 +1,5 @@
+;;----------------------------------------------------------------------------
+;; Homebrew path
+;;----------------------------------------------------------------------------
+(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
